66d01434624110676a00fefecee87405f227aa8c
Sumi Yuan <sumi@Sumis-MacBook-Pro-2.local>
2025-08-01 18:02:25 -0400

    added background blobs and changed the contact me to my resume

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,11 +5,17 @@ const Hero = forwardRef<HTMLDivElement>((props, ref) => {
 
   return (
     <div ref={ref} className='h-screen w-full pt-20 relative'>
-      <div className='blob w-80 h-80 bg-blue-600 rounded-full absolute z-0 top-1/3 left-1/5 filter blur-3xl opacity-60 animate-blob mix-blend-multiply'></div>
-      <div className='blob w-60 h-60 bg-blue-600 rounded-full absolute z-0 top-2/3 left-3/4 filter blur-3xl opacity-60 animate-blob'></div>
-      <div className='blob w-60 h-60 bg-violet-500 rounded-full absolute z-0 top-2/3 left-3/4 filter blur-3xl opacity-60 animate-blob2 '></div>
-      <div className='blob w-60 h-60 bg-blue-600 rounded-full absolute z-0 top-1/3 left-1/5 filter blur-3xl opacity-60 animate-blob2'></div>
-      <div className='flex items-center justify-center h-full relative z-10'>
+        {/* Left Blob */}
+      <div className='blob w-90 h-90 bg-pink-300 rounded-full absolute z-0 top-1/3 left-1/6 filter blur-3xl opacity-60 animate-blob'></div>
+
+
+      <div className='blob w-130 h-130 bg-blue-600 rounded-full absolute z-0 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 filter blur-3xl opacity-50 animate-blob3'></div>
+
+      {/* Right Blob */}
+      <div className='blob w-90 h-90 bg-violet-400 rounded-full absolute z-0 top-2/3 left-3/5 filter blur-3xl opacity-60 animate-blob2'></div>
+
+      {/* Content */}
+      <div className='flex items-center justify-center h-full relative z-10 px-4'>
         <div className='text-center text-white'>
           <h1 className='text-6xl font-bold mb-6 text-indigo-300'>Hello, I'm Sunny Yuan</h1>
           <p className='text-xl mb-8'>I like to build meaningful and impactful products.</p>
@@ -24,8 +30,8 @@ const Hero = forwardRef<HTMLDivElement>((props, ref) => {
               <Icons names="instagram" />
             </a>
           </div>
-          <a className='bg-white/20 backdrop-blur-sm text-white px-8 py-3 rounded-full border border-white/30 hover:bg-white/30 transition-all duration-200' href="mailto:sunny.yuan0333@gmail.com">
-            Contact Me
+          <a className='bg-white/20 backdrop-blur-sm text-white px-8 py-3 rounded-full border border-white/30 hover:bg-white/30 transition-all duration-200' href="/Resume2025.pdf">
+            My Resume
           </a>
         </div>
       </div>
